ecbee6e23eb574ea49496a7dca02d13a9add267b
Karthik2056426 <karthikassasins@gmail.com>
2025-07-18 11:25:36 +0530

    color update

--- src/components/CarouselView.tsx
+++ src/components/CarouselView.tsx
@@ -62,10 +62,10 @@ const CarouselView = ({ onBack }: CarouselViewProps) => {
 
   const getHouseColor = (house: string) => {
     const colors = {
-      'Delany': 'bg-green-300 text-green-800 border-green-200',
-      'Gandhi': '<bg-yellow-300 text-yellow-800 border-yellow-200',
-      'Tagore': 'bg-blue-300 text-blue-800 border-blue-200',
-      'Aloysius': 'bg-red-300 text-red-800 border-red-200'
+      'Delany': 'bg-green-100 text-green-800 border-green-200',
+      'Gandhi': 'bg-yellow-100 text-yellow-800 border-yellow-200',
+      'Tagore': 'bg-blue-100 text-blue-800 border-blue-200',
+      'Aloysius': 'bg-red-100 text-red-800 border-red-200'
     };
     return colors[house as keyof typeof colors] || 'bg-gray-100 text-gray-800';
   };
@@ -125,16 +125,16 @@ const CarouselView = ({ onBack }: CarouselViewProps) => {
                     let houseClasses = '';
                     switch (house.name) {
                       case 'Tagore':
-                        houseClasses = 'bg-blue-100 text-blue-800 border-blue-200';
+                        houseClasses = 'bg-[#38b6ff] text-white border-blue-200';
                         break;
                       case 'Aloysius':
-                        houseClasses = 'bg-red-100 text-red-800 border-red-200';
+                        houseClasses = 'bg-[#ed6258] text-white border-red-200';
                         break;
                       case 'Gandhi':
-                        houseClasses = 'bg-yellow-100 text-yellow-800 border-yellow-200';
+                        houseClasses = 'bg-[#ffde59] text-white border-yellow-200';
                         break;
                       case 'Delany':
-                        houseClasses = 'bg-green-100 text-green-800 border-green-200';
+                        houseClasses = 'bg-[#00bf63] text-white border-green-200';
                         break;
                       default:
                         houseClasses = 'bg-white text-gray-800 border-gray-200';
